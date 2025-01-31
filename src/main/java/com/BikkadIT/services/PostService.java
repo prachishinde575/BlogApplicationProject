d9947102353a0cf41d28d4ee85e360ab4a42dffe
prachishinde575 <prachishinde575@gmail.com>
2025-02-01 00:33:18 +0530

    Code upto pegination

--- src/main/java/com/BikkadIT/services/PostService.java
+++ src/main/java/com/BikkadIT/services/PostService.java
@@ -20,7 +20,7 @@ public interface PostService {
 	
 	//get all posts
 	
-	List<PostDto> getAllPost();
+	List<PostDto> getAllPost(Integer pageNumber, Integer pageSize);
 	
 	//get single post
 	
--- src/main/java/com/BikkadIT/services/impl/PostServiceImpl.java
+++ src/main/java/com/BikkadIT/services/impl/PostServiceImpl.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import com.BikkadIT.entities.Category;
 import com.BikkadIT.entities.Post;
@@ -71,9 +74,17 @@ public class PostServiceImpl implements PostService {
 	}
 
 	@Override
-	public List<PostDto> getAllPost() {
-			List<Post> allPosts = this.postRepo.findAll();
-			List<PostDto> postDtos = allPosts.stream().map((post) -> this.modelMapper.map(post,PostDto.class)).collect(Collectors.toList());
+	public List<PostDto> getAllPost(Integer pageNumber, Integer pageSize) {
+			
+	//	int pageSize = 5;
+	//  int pageNumber = 1;
+		Pageable p = PageRequest.of(pageNumber, pageSize);
+		
+		Page<Post> pagePost = this.postRepo.findAll(p);
+		List<Post> allPosts = pagePost.getContent();
+		
+		
+		List<PostDto> postDtos = allPosts.stream().map((post) -> this.modelMapper.map(post,PostDto.class)).collect(Collectors.toList());
 			
 
 		return postDtos;
